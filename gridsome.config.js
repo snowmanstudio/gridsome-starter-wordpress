2d6cff6b3183275d8098ed7935059f49620939ab
snowmanstudio <bloggertanim@outlook.com>
2023-01-06 09:25:23 +0600

    mards

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,14 +5,14 @@ module.exports = {
 
   templates: {
     WordPressCategory: '/category/:slug', // adds a route for the "category" post type (Optional)
-    WordPressPost: '/:slug', // adds a route for the "post" post type (Optional)
+    WordPressPost: '/:title', // adds a route for the "post" post type (Optional)
     BlogPost:'/:slug',
   },
   plugins: [
     {
       use: '@gridsome/source-wordpress',
       options: {
-        baseUrl: 'https://sajidch.com', // required
+        baseUrl: 'https://freehub.online', // required
         apiBase: 'wp-json',
         typeName: 'WordPress', // GraphQL schema name (Optional)
       }
