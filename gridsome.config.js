e6f97f25a9b52a9a89c22e59b3132140b33774ef
snowmanstudio <bloggertanim@outlook.com>
2023-01-06 13:10:49 +0600

    title to id

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,6 +6,7 @@ module.exports = {
     WordPressCategory: '/category/:slug', // adds a route for the "category" post type (Optional)
     WordPressPost: '/:year/:month/:day/:slug', // adds a route for the "post" post type (Optional)
     WordPressPostTag: '/tag/:slug', // adds a route for the "post_tag" post type (Optional)
+    BlogPost: '/blog/:id',
     
   },
   plugins: [
