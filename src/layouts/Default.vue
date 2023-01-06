55605b0390a35f96796b98669b077210958f2684
snowmanstudio <bloggertanim@outlook.com>
2023-01-06 12:05:09 +0600

    urala

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,36 +2,30 @@
   <div class="layout">
     <header class="header">
       <strong>
-        <g-link to="/">Gridsome</g-link>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
     </header>
     <slot/>
   </div>
 </template>
 
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
 <style>
 body {
   font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  margin:0;
+  padding:0;
+  line-height: 1.5;
 }
-
-img {
-  max-width: 100%;
-}
-
-nav[role="navigation"] {
-  text-align: center;
-}
-nav[role="navigation"] a {
-  display: inline-block;
-  margin: 1.0em 0.75em 2.0em;
-}
-
 .layout {
-  max-width: 600px;
+  max-width: 760px;
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
@@ -39,16 +33,11 @@ nav[role="navigation"] a {
 .header {
   display: flex;
   justify-content: space-between;
+  align-items: center;
   margin-bottom: 20px;
+  height: 80px;
 }
 .nav__link {
   margin-left: 20px;
-}
-.post-list {
-  list-style: none;
-  padding-left: 0;
-}
-.post-list li {
-  padding: 1em 0;
 }
 </style>
