0b5fe2491e2afbf164b54a7832daa3b7ec4290db
snowmanstudio <bloggertanim@outlook.com>
2023-01-06 10:47:37 +0600

    please work

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   templates: {
     WordPressCategory: '/category/:slug', // adds a route for the "category" post type (Optional)
-    WordPressPost: '/:slug', // adds a route for the "post" post type (Optional)
+    WordPressPost: '/blog/:slug', // adds a route for the "post" post type (Optional)
     BlogPost:'/:title',
   },
   plugins: [
